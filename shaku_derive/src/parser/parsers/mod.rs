f3833154718de560e66d1f7992c29d56fc940faf
Mcat12 <newtoncat12@yahoo.com>
2020-01-26 17:00:52 -0500

    Rewrite main lib.rs documentation
    Also updated the autofac test to be simpler, and ran rustfmt.

--- shaku_derive/src/parser/parsers/mod.rs
+++ shaku_derive/src/parser/parsers/mod.rs
@@ -1,7 +1,7 @@
 mod identifier_from_derive_input;
 mod metadata_from_derive_input;
+mod path_key_value;
 mod property_from_derive_input;
 mod property_from_field;
-mod path_key_value;
 
 pub use path_key_value::PathKeyValue;
