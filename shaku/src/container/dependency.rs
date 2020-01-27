d9cdca03167ff6bc8fad6888b36fd8c3861cc4e5
Mcat12 <newtoncat12@yahoo.com>
2020-01-27 17:45:01 -0500

    Document Dependency

--- shaku/src/container/dependency.rs
+++ shaku/src/container/dependency.rs
@@ -1,5 +1,7 @@
 use std::any::{type_name, TypeId};
 
+/// Represents a component dependency. `Dependency` is used to determine the
+/// build order of components (build only after dependencies are built).
 #[derive(Debug)]
 pub struct Dependency {
     pub(crate) type_id: TypeId,
@@ -8,6 +10,9 @@ pub struct Dependency {
 }
 
 impl Dependency {
+    /// Create a `Dependency` instance representing a dependency on `T`. The
+    /// `name` field is the name of the attribute in the component struct where
+    /// the dependency is to be injected, for debugging purposes.
     pub fn new<T: ?Sized + 'static>(name: String) -> Self {
         Dependency {
             type_id: TypeId::of::<T>(),
