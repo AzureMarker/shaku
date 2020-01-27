e68a06e5613aba279ac405e26024c86ebe401769
Mcat12 <newtoncat12@yahoo.com>
2020-01-26 20:46:49 -0500

    Update ContainerBuilder documentation

--- shaku/src/container/container_builder.rs
+++ shaku/src/container/container_builder.rs
@@ -1,5 +1,3 @@
-//! Implementation of a `ContainerBuilder`
-
 use std::any::{type_name, TypeId};
 use std::collections::HashMap;
 
@@ -8,15 +6,16 @@ use crate::container::{Container, ContainerBuildContext, RegisteredType};
 use crate::result::Result as DIResult;
 use crate::Dependency;
 
-/// Build a [Container](struct.Container.html) registering components
-/// with or without parameters.
+/// Registers components in order to build a [`Container`].
 ///
-/// Once finished, you have to call [build()](struct.ContainerBuilder.html#method.build)
-/// to build the associated `Container`. This method can Err if you tried to register
-/// invalid values.
+/// Once finished, call [`build`] to build the [`Container`].
 ///
-/// See [module documentation](index.html) or
-/// [ContainerBuilder::build()](struct.ContainerBuilder.html#method.build) for more details.
+/// See [module documentation] or [`ContainerBuilder::build`] for more details.
+///
+/// [`Container`]: struct.Container.html
+/// [`build`]: #method.build
+/// [module documentation]: index.html
+/// [`ContainerBuilder::build`]: #method.build
 pub struct ContainerBuilder {
     registration_map: HashMap<TypeId, RegisteredType>,
 }
@@ -38,11 +37,12 @@ impl ContainerBuilder {
     /// Register a new component with this builder.
     /// If that component was already registered, the old Component is replaced.
     ///
-    /// This method returns a mutable [RegisteredType](struct.RegisteredType.html)
-    /// allowing to chain calls to
-    /// [with_named_parameter()](struct.RegisteredType.html#method.with_named_parameter)
-    /// or [with_typed_parameter()](struct.RegisteredType.html#method.with_typed_parameter)
-    /// to add parameters to be used to instantiate this Component.
+    /// This method returns a mutable [`RegisteredType`], allowing you to chain
+    /// calls to [`with_named_parameter`] or [`with_typed_parameter`].
+    ///
+    /// [`RegisteredType`]: struct.RegisteredType.html
+    /// [`with_named_parameter`]: struct.RegisteredType.html#method.with_named_parameter
+    /// [`with_typed_parameter`]: struct.RegisteredType.html#method.with_typed_parameter
     pub fn register_type<C: Component>(&mut self) -> &mut RegisteredType {
         self.register_lambda::<C::Interface>(
             type_name::<C>(),
@@ -54,17 +54,17 @@ impl ContainerBuilder {
     /// Register a new component with this builder.
     /// If that component was already registered, the old Component is replaced.
     ///
-    /// This register method is an alternative to implementing [Component].
+    /// This register method is an alternative to implementing [`Component`].
     /// This may be useful in cases such as using a mock or dynamically choosing the
     /// implementation based on dependencies.
     ///
-    /// This method returns a mutable [RegisteredType](struct.RegisteredType.html)
-    /// allowing to chain calls to
-    /// [with_named_parameter()](struct.RegisteredType.html#method.with_named_parameter)
-    /// or [with_typed_parameter()](struct.RegisteredType.html#method.with_typed_parameter)
-    /// to add parameters to be used to instantiate this Component.
+    /// This method returns a mutable [`RegisteredType`], allowing you to chain
+    /// calls to [`with_named_parameter`] or [`with_typed_parameter`].
     ///
-    /// [Component]: ../component/trait.Component.html
+    /// [`Component`]: ../component/trait.Component.html
+    /// [`RegisteredType`]: struct.RegisteredType.html
+    /// [`with_named_parameter`]: struct.RegisteredType.html#method.with_named_parameter
+    /// [`with_typed_parameter`]: struct.RegisteredType.html#method.with_typed_parameter
     pub fn register_lambda<I: Interface + ?Sized>(
         &mut self,
         component_name: &str,
@@ -95,21 +95,19 @@ impl ContainerBuilder {
         self.registration_map.get_mut(&interface_type_id).unwrap()
     }
 
-    /// Parse this `ContainerBuilder` content to check if all the registrations are valid.
-    /// If so, consume this `ContainerBuilder` to build a [Container]. The
-    /// [ContainerBuildContext] struct will be used to build the [Container].
+    /// Consume this `ContainerBuilder` to build a [`Container`]. The
+    /// [`ContainerBuildContext`] struct will be used to build the [`Container`].
     /// The components are built at this time.
     ///
-    /// [Container]: struct.Container.html
-    /// [ContainerBuildContext]: struct.ContainerBuildContext.html
+    /// [`Container`]: struct.Container.html
+    /// [`ContainerBuildContext`]: struct.ContainerBuildContext.html
     ///
     /// # Errors
-    /// The components are built at this time, so any dependency or parameter errors will be
-    /// returned here.
+    /// The components are built at this time, so any dependency or parameter
+    /// errors will be returned here.
     ///
-    /// # Examples
-    ///
-    /// ```rust
+    /// # Example
+    /// ```
     /// use shaku::{Component, Error as DIError, Interface};
     ///
     /// trait Foo: Interface { fn foo(&self); }
@@ -131,27 +129,30 @@ impl ContainerBuilder {
     /// impl FooDuplicate for FooDuplicateImpl1 { fn foo(&self) -> String { "FooDuplicateImpl1".to_string() } }
     /// impl FooDuplicate for FooDuplicateImpl2 { fn foo(&self) -> String { "FooDuplicateImpl2".to_string() } }
     ///
-    /// let mut builder = shaku::ContainerBuilder::new();
+    /// {
+    ///     // Valid registration
+    ///     let mut builder = shaku::ContainerBuilder::new();
+    ///     builder.register_type::<FooImpl>();
     ///
-    /// // Valid registration
-    /// builder.register_type::<FooImpl>();
+    ///     let container = builder.build();
+    ///     assert!(container.is_ok());
+    ///     let foo = container.unwrap().resolve::<dyn Foo>();
+    ///     assert!(foo.is_ok());
+    /// }
     ///
-    /// let container = builder.build();
-    /// assert!(container.is_ok());
-    /// let foo = container.unwrap().resolve::<dyn Foo>();
-    /// assert!(foo.is_ok());
+    /// {
+    ///     // Duplicate registration, only the latest component registered is kept
+    ///     let mut builder = shaku::ContainerBuilder::new();
+    ///     builder.register_type::<FooDuplicateImpl1>();
+    ///     builder.register_type::<FooDuplicateImpl2>();
     ///
-    /// // Invalid registration, duplicate => only the latest Component registered is kept
-    /// let mut builder = shaku::ContainerBuilder::new();
-    /// builder.register_type::<FooDuplicateImpl1>();
-    /// builder.register_type::<FooDuplicateImpl2>();
-    ///
-    /// let container = builder.build();
-    /// assert!(container.is_ok());
-    /// let mut container = container.unwrap();
-    /// let foo = container.resolve::<dyn FooDuplicate>();
-    /// assert!(foo.is_ok());
-    /// assert_eq!(foo.unwrap().foo(), "FooDuplicateImpl2".to_string());
+    ///     let container = builder.build();
+    ///     assert!(container.is_ok());
+    ///     let mut container = container.unwrap();
+    ///     let foo = container.resolve::<dyn FooDuplicate>();
+    ///     assert!(foo.is_ok());
+    ///     assert_eq!(foo.unwrap().foo(), "FooDuplicateImpl2".to_string());
+    /// }
     /// ```
     pub fn build(self) -> DIResult<Container> {
         ContainerBuildContext::new(self.registration_map).build()
