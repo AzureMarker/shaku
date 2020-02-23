a7e31aae794e008e8c7ddaff96585029866439e0
Mcat12 <newtoncat12@yahoo.com>
2020-02-22 21:13:51 -0500

    Remove unused Error::Registration
    Compile time DI kind of removes the reason for its existence.

--- shaku/src/error.rs
+++ shaku/src/error.rs
@@ -7,8 +7,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Clone, Debug, PartialEq)]
 #[non_exhaustive]
 pub enum Error {
-    /// Error while registering a component/provider
-    Registration(String),
     /// Error while resolving a component
     ResolveError(String),
 }
@@ -16,7 +14,6 @@ pub enum Error {
 impl StdError for Error {
     fn description(&self) -> &str {
         match self {
-            Error::Registration(msg) => msg,
             Error::ResolveError(msg) => msg,
         }
     }
@@ -29,7 +26,6 @@ impl StdError for Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::Registration(msg) => write!(f, "Registration Error: {}", msg),
             Error::ResolveError(msg) => write!(f, "Resolve Error: {}", msg),
         }
     }
