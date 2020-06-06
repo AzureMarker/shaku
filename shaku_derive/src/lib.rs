ea52947f54c11f843656c5c4da8e18b6ca602091
Mcat12 <newtoncat12@yahoo.com>
2020-06-06 12:41:09 -0400

    Stub out procedural module macro and refactor root-level error handling

--- shaku_derive/src/lib.rs
+++ shaku_derive/src/lib.rs
@@ -4,6 +4,7 @@ extern crate proc_macro;
 #[macro_use]
 extern crate quote;
 
+use crate::error::Error;
 use proc_macro::TokenStream;
 
 mod common_output;
@@ -11,6 +12,7 @@ mod component;
 mod consts;
 mod debug;
 mod error;
+mod module_macro;
 mod parser;
 mod provider;
 mod structures;
@@ -19,30 +21,30 @@ mod structures;
 pub fn component(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    match component::expand_derive_component(&input) {
-        Ok(tokenstream) => tokenstream,
-        Err(error) => {
-            let msg = error.to_string();
-            quote! {
-                compile_error!(#msg);
-            }
-        }
-    }
-    .into()
+    component::expand_derive_component(&input)
+        .unwrap_or_else(make_compile_error)
+        .into()
 }
 
 #[proc_macro_derive(Provider, attributes(shaku))]
 pub fn provider(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    match provider::expand_derive_provider(&input) {
-        Ok(tokenstream) => tokenstream,
-        Err(error) => {
-            let msg = error.to_string();
-            quote! {
-                compile_error!(#msg);
-            }
-        }
+    provider::expand_derive_provider(&input)
+        .unwrap_or_else(make_compile_error)
+        .into()
+}
+
+#[proc_macro]
+pub fn module(input: TokenStream) -> TokenStream {
+    module_macro::expand_module_macro(input.into())
+        .unwrap_or_else(make_compile_error)
+        .into()
+}
+
+fn make_compile_error(error: Error) -> proc_macro2::TokenStream {
+    let msg = error.to_string();
+    quote! {
+        compile_error!(#msg);
     }
-    .into()
 }
---
+++ shaku_derive/src/module_macro.rs
@@ -0,0 +1,6 @@
+use crate::error::Error;
+use proc_macro2::TokenStream;
+
+pub fn expand_module_macro(input: TokenStream) -> Result<TokenStream, Error> {
+    unimplemented!()
+}
